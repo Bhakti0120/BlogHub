ca903cda8646370522f993a76a54128772a618c4
Bhakti <bhaktipatil2003@gmail.com>
2025-11-24 07:47:34 +0530

    Backend Setup

---
+++ api/index.js
@@ -0,0 +1,31 @@
+import express from "express";
+import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
+import cors from "cors";
+import mongoose from "mongoose";
+dotenv.config();
+
+const PORT = process.env.PORT;
+const app = express();
+
+app.use(cookieParser());
+app.use(express.json());
+app.use(
+  cors({
+    origin: process.env.FRONTEND_URL,
+    credentials: true,
+  })
+);
+
+mongoose
+  .connect(process.env.MONGODB_CONN, { dbName: "BlogHubDB" })
+  .then(() => {
+    console.log("Database connected");
+  })
+  .catch((err) => {
+    console.log("Database connection failed:", err);
+  });
+
+app.listen(PORT, () => {
+  console.log("Server is running on port:", PORT);
+});
